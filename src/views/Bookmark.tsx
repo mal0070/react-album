273723d07cb81dfa8210af0d79555376403be6ee
Mina Lee <mal0070@naver.com>
2024-11-18 17:28:17 +0900

    feat: render bookmark page

--- src/views/Bookmark.tsx
+++ src/views/Bookmark.tsx
@@ -1,7 +1,51 @@
+import { Header } from '@/components/common';
+import { ImageCard } from '@/components/home/image-card';
+import { ImageCardType } from '@/types';
+import { useEffect, useState } from 'react';
+
 function Bookmark() {
+  const [bookmarks, setBookmarks] = useState<ImageCardType[]>([]);
+
+  const getLocalStorage = () => {
+    const storedImages = localStorage.getItem('bookmark');
+    let bookmarks: ImageCardType[] = [];
+
+    if (storedImages) {
+      try {
+        bookmarks = JSON.parse(storedImages);
+      } catch (error) {
+        console.error('데이터 파싱 오류: ', error);
+        bookmarks = [];
+      }
+    }
+
+    if (bookmarks.length === 0) {
+      console.log('저장된 데이터 없음');
+    } else {
+      setBookmarks([...bookmarks]);
+    }
+  };
+
+  useEffect(() => {
+    getLocalStorage();
+  }, []);
+
   return (
-    <div>Bookmark</div>
-  )
+    <div className="page">
+      <div className="page__container">
+        <Header />
+        {bookmarks.length ? (
+          <div className="page__container__contents">
+            {bookmarks.map((image: ImageCardType) => {
+              return <ImageCard data={image} key={image.id} />;
+            })}
+          </div>
+        ) : (
+          <div className='w-full flex items-center justify-center mt-24 text-xl'>조회 가능한 데이터가 없습니다.</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export {Bookmark}
+export { Bookmark };
